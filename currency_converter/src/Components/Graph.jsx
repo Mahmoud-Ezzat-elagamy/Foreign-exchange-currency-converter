206b3205eca163b70f3bc7ec4549d06f4ab17933
mahmoud ezzat <mahmoud.ezzat1234123121@gmail.com>
2026-07-06 21:16:41 +0300

    collect the data for the graph

--- currency_converter/src/Components/Graph.jsx
+++ currency_converter/src/Components/Graph.jsx
@@ -1,5 +1,57 @@
+import { Line } from "react-chartjs-2";
+import "chart.js/auto";
+import { getRatesForTimeFrame } from "../helpers/helper";
+import { useCurrency } from "../contextApi/concurrs";
+import { useViewContext } from "../contextApi/currentView";
+import { useEffect, useState } from "react";
+
+const timeFrames = {
+  "1D": "1",
+  "1W": "7",
+  "1M": "30",
+  "6M": "180",
+  "1Y": "365",
+};
+
 function Graph() {
-  return <div></div>;
+  const [data, setData] = useState(null);
+  const { state } = useCurrency();
+  const { timeframe } = useViewContext();
+  const { selectedSendCurrency, selectedReceiveCurrency } = state;
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getRatesForTimeFrame(
+        selectedSendCurrency,
+        selectedReceiveCurrency,
+        timeFrames[timeframe],
+      );
+      setData(data);
+    }
+    fetchData();
+  }, [selectedSendCurrency, selectedReceiveCurrency, timeframe]);
+  console.log(data);
+  return (
+    <div>
+      {/* <Line
+        datasetIdKey="id"
+        data={{
+          labels: ["Jun", "Jul", "Aug"],
+          datasets: [
+            {
+              id: 1,
+              label: "",
+              data: [5, 6, 7],
+            },
+            {
+              id: 2,
+              label: "",
+              data: [3, 2, 1],
+            },
+          ],
+        }}
+      /> */}
+    </div>
+  );
 }
 
 export default Graph;
--- currency_converter/src/helpers/helper.js
+++ currency_converter/src/helpers/helper.js
@@ -8,3 +8,32 @@ export async function getRate(fromCurrency, toCurrency) {
   const [data] = await res.json();
   return data.rate;
 }
+
+export async function getRatesForTimeFrame(
+  fromCurrency,
+  toCurrency,
+  timeframe,
+) {
+  const dayseconds = 24 * 60 * 60 * 1000;
+  const today = getDateFormated(new Date().toLocaleDateString());
+  const startDate = getDateFormated(
+    new Date(Date.now() - dayseconds * timeframe).toLocaleDateString(),
+  );
+
+  console.log(
+    `https://api.frankfurter.dev/v2/rates?from=${startDate}&to=${today}&quotes=${toCurrency}&base=${fromCurrency}`,
+  );
+
+  const res = await fetch(
+    `https://api.frankfurter.dev/v2/rates?from=${startDate}&to=${today}&quotes=${toCurrency}&base=${fromCurrency}`,
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch exchange rates for timeframe");
+  }
+  return await res.json();
+}
+
+function getDateFormated(date) {
+  const [month, day, year] = date.split("/");
+  return `${year}-${month}-${day}`;
+}
