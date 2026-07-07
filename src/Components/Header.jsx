374bfd832f7bba2910aef6b42fada1f75c4d97b2
mahmoud ezzat <mahmoud.ezzat1234123121@gmail.com>
2026-07-08 02:30:28 +0300

    add the number of supported currencies

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
+import { validCurrencies } from "../../public/data";
+
 function Header() {
   return (
     <div className="flex justify-between items-center bg-neutral-900 px-6 py-5">
       <img src="/logo.svg" alt="FX_CHECKER" />
       <div className="uppercase flex text-neutral-200 ">
-        XX currendies <span className="mx-1 text-xl -translate-y-1.5">.</span>
+        {validCurrencies.length} currencies{" "}
+        <span className="mx-1 text-xl -translate-y-1.5">.</span>
         eod <span className="mx-1 text-xl -translate-y-1.5">.</span> ecb data
       </div>
     </div>
